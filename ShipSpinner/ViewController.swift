320c901ac48eb18afcbd4daaba50c466d10fedbb
James Tan <jamesktan@gmail.com>
2014-11-16 23:18:26 +0200

    init

---
+++ ShipSpinner/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ShipSpinner
+//
+//  Created by James Tan on 11/16/14.
+//  Copyright (c) 2014 Axon Flux. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myscene: SCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        myscene.scene = SCNScene(named: "hurricane.dae")
+        myscene.allowsCameraControl = true;
+        myscene.backgroundColor = UIColor.lightGrayColor()
+
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
