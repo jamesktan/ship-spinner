76eddbc1275abdda58ea8d944914d24599c84cea
James Tan <jamesktan@gmail.com>
2015-04-06 08:55:12 +0300

    added a new util class to handle the download and unzipping

---
+++ ShipSpinner/Util.swift
@@ -0,0 +1,21 @@
+//
+//  Util.swift
+//  ShipSpinner
+//
+//  Created by James Tan on 4/6/15.
+//  Copyright (c) 2015 Axon Flux. All rights reserved.
+//
+
+import UIKit
+
+class Util: NSObject {
+    class func downloadFileAtPath(url: NSString) -> NSString {
+        var filePath : NSString = ""
+        return filePath
+    }
+    
+    class func downloadModelAndUnzipAtPath(url: NSString) -> NSString {
+        var filePath : NSString = ""
+        return filePath
+    }
+}
