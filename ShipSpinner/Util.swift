f727b0fc464c6cb1361726b668939dfec559ae02
James Tan <jamesktan@gmail.com>
2015-04-06 09:06:29 +0300

    Moved the path utility functions and completed the plist download code

--- ShipSpinner/Util.swift
+++ ShipSpinner/Util.swift
@@ -9,8 +9,24 @@
 import UIKit
 
 class Util: NSObject {
+    class func getDownloadPath() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+        let pathDownloads = paths.stringByAppendingPathComponent("Downloads") as NSString
+        return pathDownloads
+    }
+    
+    class func getPath( fileName: NSString) -> NSString {
+        return NSBundle.mainBundle().pathForResource(fileName, ofType: "plist")!
+    }
+    class func getPathJPG(fileName: NSString)->NSString {
+        return NSBundle.mainBundle().pathForResource(fileName, ofType: "jpg")!
+    }
+    
     class func downloadFileAtPath(url: NSString) -> NSString {
-        var filePath : NSString = ""
+        var urlObj : NSURL = NSURL(string: url)!
+        var urlData : NSData = NSData(contentsOfURL: urlObj)!
+        var filePath = Util.getDownloadPath().stringByAppendingPathComponent(url.lastPathComponent)
+        urlData.writeToFile(filePath, atomically: true)
         return filePath
     }
     
