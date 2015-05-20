bc569327fe2bfef2258f86386d60fcfb6aaeb69f
James Tan <jamesktan@gmail.com>
2015-05-20 18:14:43 +0300

    Finagling with constraints and added purchase behavior

--- ShipSpinner/PurchaseView.swift
+++ ShipSpinner/PurchaseView.swift
@@ -21,6 +21,22 @@ class PurchaseView: UIViewController {
       // Dispose of any resources that can be recreated.
   }
   
+  @IBAction func buyRemastered(sender: AnyObject) {
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.daledietrich.com"]];
+    
+    UIApplication.sharedApplication().openURL(NSURL(string: "http://www.amazon.com/gp/product/B00TUIGLLU/ref=as_li_qf_sp_asin_il_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00TUIGLLU&linkCode=as2&tag=kawila-20&linkId=UUZFVQIKRUJP3R7H")!)
+
+  }
+  
+  @IBAction func buyCollector(sender: AnyObject) {
+    UIApplication.sharedApplication().openURL(NSURL(string: "http://www.amazon.com/gp/product/B00K6ZUOQE/ref=as_li_qf_sp_asin_il_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00K6ZUOQE&linkCode=as2&tag=kawila-20&linkId=5QY2TZU3QTDVE6KT")!)
+
+  }
+  
+  @IBAction func buyArt(sender: AnyObject) {
+    UIApplication.sharedApplication().openURL(NSURL(string: "http://www.amazon.com/gp/product/0985902248/ref=as_li_qf_sp_asin_il_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0985902248&linkCode=as2&tag=kawila-20&linkId=QMWLXOCEG3RSZP7B")!)
+
+  }
   
   @IBAction func launchWeb(sender: UIButton) {
     
